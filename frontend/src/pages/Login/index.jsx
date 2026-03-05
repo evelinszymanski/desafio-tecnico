424c8c9367a6de9d1f267ecb29a7d1cc2529bb9b
Evelin Szymanski <evelinszy@gmail.com>
2026-03-05 20:12:12 -0300

    refactor: Ajuste na tela de login para utilização de componente nos campos do formulário

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useAuth } from "../../context/AuthContext";
 import { toast } from "react-toastify";
-import { Box, Container, Icon, IconButton, InputAdornment, Typography } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Logo from '../../components/Logo';
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { Box, Container, Typography, Stack } from '@mui/material';
+import Logo from '../../components/Logo';
+import Button from "../../components/Button";
+import RHFInput from "../../components/RHFInput";
 
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { control, handleSubmit } = useForm();
     const { login } = useAuth();
     const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false);
 
     const onSubmit = handleSubmit(async (data) => {
         try {
@@ -64,39 +61,26 @@ const Login = () => {
                         </Box>
                         <Box component="form" onSubmit={onSubmit}>
                             <Stack spacing={4} justifyContent="center">
-                                <TextField 
-                                    {...register("email", {
+                                <RHFInput 
+                                    control={control}
+                                    name="email"
+                                    label="Email"
+                                    type="text"
+                                    rules={{ 
                                         required: "Email é obrigatório",
                                         pattern: {
                                             value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                                            message: "Digite um email válido"
+                                            message: "Email inválido"
                                         }
-                                    })}
-                                    label="Email"
-                                    error={!!errors.email}
-                                    helperText={errors.email?.message}
-                                />
-                                <TextField 
-                                    {...register("password", { 
-                                        required: "Senha é obrigatória",
-                                    })}
-                                    label="Senha"
-                                    error={!!errors.password}
-                                    helperText={errors.password?.message}
-                                    type={showPassword ? 'text' : 'password'}
-                                    slotProps={{
-                                        input: {
-                                            endAdornment: (
-                                                <InputAdornment position="end">
-                                                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                                                        {showPassword ? <Icon color="primary">visibility_off</Icon> : <Icon color="primary">visibility</Icon>}
-                                                    </IconButton>
-                                                </InputAdornment>
-                                            ),
-                                        },
                                     }}
                                 />
-                                <Button type="submit" variant="contained" size='large' sx={{ fontWeight: 700 }}>
+                                <RHFInput 
+                                    control={control}
+                                    name="password"
+                                    label="Senha"
+                                    type="password"
+                                />
+                                <Button type="submit" size='large'>
                                     Entrar
                                 </Button>
                             </Stack>
