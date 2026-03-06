ec9336237559de77ada5111193f56ccef9adc86b
Evelin Szymanski <evelinszy@gmail.com>
2026-03-06 15:56:29 -0300

    fix: Correção para importar corretamente a tradução pt-br para o Moment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,17 +7,17 @@ import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { ToastContainer } from 'react-toastify';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import 'moment/locale/pt-br';
+import 'moment/dist/locale/pt-br';
 import moment from 'moment';
 import { theme as customTheme } from './styles/theme.js';
 
 const theme = createTheme(customTheme);
-moment.locale('pt-br');
+moment.locale("pt-br");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale='pt-br'>
+      <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="pt-br">
         <AuthProvider>
           <RouterProvider router={router} />
           <ToastContainer theme='colored' closeOnClick autoClose={2000}/>
